c945ec018564b32291dd2459d50e7e1e164379f5
Alex Stupakov <stupakov@gmail.com>
2014-01-18 14:20:51 -0800

    add dropdown for choosing dataset to plot
    add convolution filtering for plotting smooth line representing datastream

--- README.md
+++ README.md
@@ -4,9 +4,23 @@
 - fetches data from google docs spreadsheet
 
 ### TODO
-- list of datastreams
-- line charts of all data streams (select them?)
-- calendar view of data: http://bl.ocks.org/mbostock/4063318
+- multiple independent graphs, each can pick their own dataset
+- fix plotting of dots for zero values
 - scatter plot of 2 data streams
 - filter data by days of week
+- move filter into an object
+- group data into week/month groups (different than a week-long convolution filter)
+- enable/disable filtered datastream
+- calendar view of data: http://bl.ocks.org/mbostock/4063318
+- can choose filter properties for graph
+- different graph (bar?) for yes/no data
 
+### Integrations
+- SleepCycle
+- Strava workouts
+- Runkeeper workouts
+- Crunch Gym visits
+
+### Links
+- bar charts with D3 - http://alignedleft.com/tutorials/d3/making-a-bar-chart
+- circle graphs of activity over time in categories - http://neuralengr.com/asifr/journals/
--- personal-data.js
+++ personal-data.js
@@ -1,15 +1,16 @@
 var useCachedData = true;
+var allData;
 
 var config = {
   feed: "list",
   scopes: 'https://spreadsheets.google.com/feeds'
 };
 
-(function () {
+$(function () {
   if(useCachedData) {
     parseSpreadsheetData(cachedData);
   }
-})();
+});
 
 // Functions for fetching data from google docs
 function handleClientLoad() {
@@ -42,29 +43,42 @@ function loadClient() {
 }
 
 // Parse and process a dataset and create graphs
-function parseSpreadsheetData(data) {
-  var model = new Tabletop.Model( { data: data,
+function parseSpreadsheetData(dataset) {
+  var model = new Tabletop.Model( { data: dataset,
                                     parseNumbers: false,
                                     postProcess: false,
                                     tabletop: undefined } );
 
-  var property = "happiness";
-
   // Reformat the data
-  var data = model.elements.map(function(element) {
-    return reformat(element, property);
+  allData = model.elements.map(function(element) {
+      return reformat(element, model.column_names);
   });
 
-  plotData(data, property);
+  populateDropdown(model.column_names);
+  $("#columns").trigger("change");
 };
 
 // Helpers
-function reformat(d, property) {
-  if (d[property] != "") {
-    d[property] = +d[property];
-  } else {
-    d[property] = undefined;
-  }
+function reformat(d, columnNames) {
+  columnNames.forEach(function(columnName) {
+    if(columnName != "date") {
+      // "y" -> 1.0
+      // "n" -> 0.0
+      // all numbers -> float
+      // everything else -> undefined
+      if(d[columnName] === "y") {
+        d[columnName] = 1.0;
+      }
+      if(d[columnName] === "n") {
+        d[columnName] = 0.0;
+      }
+      if (d[columnName] === "" || isNaN(d[columnName])) {
+        d[columnName] = undefined;
+      } else {
+        d[columnName] = +d[columnName];
+      }
+    }
+  });
   d.date = parseDate(d.date);
   return d;
 }
@@ -73,12 +87,80 @@ function parseDate(date) {
   return d3.time.format("%-m/%-d/%Y").parse(date);
 }
 
+var squareWindow = [.4,.6,.8,1,.8,.6,.4];
+
+// TODO: fix missing values at ends
+function convolve(values, windowValues, windowOffset) {
+  // initialize weights and result to array of zeros
+  var length = values.length;
+  var weights = [];
+  var result = [];
+  while(--length >= 0) {
+    weights[length] = 0;
+    result[length] = 0;
+  }
+
+  // set each value in the result to the sum of all contributing values
+  // save the total weight of contributing values
+  // then divide each sum by the total weight to get weighted value
+  values.forEach(function(value, valueIndex) {
+    windowValues.forEach(function(coefficient, windowIndex) {
+      resultIndex = valueIndex - windowIndex + windowOffset;
+      if((resultIndex < 0) ||
+         (resultIndex > values.length - 1) ||
+          "undefined" == typeof(value)
+        ) {
+        return;
+      }
+
+      weights[resultIndex] = weights[resultIndex] + coefficient;
+      result[resultIndex] = result[resultIndex] + coefficient*value;
+    })
+  });
+
+  result.forEach(function(value, index) {
+    if(weights[index] == 0) {
+      result[index] = undefined;
+    } else {
+      result[index] = result[index] / weights[index];
+    }
+  });
+
+  return result;
+}
+
+function filterData(data, property) {
+  var values = data.map(function(el) {
+    return el[property];
+  });
+
+  values = convolve(values, squareWindow, 3);
+
+  return data.map(function(el, index) {
+    return {
+      date: el.date,
+      value: values[index]
+    };
+  });
+}
+
+function pickData(data, property) {
+  return data.map(function(el) {
+    return {
+      date: el.date,
+      value: el[property]
+    };
+  });
+}
 
 // Plotting the data
-function plotData(data, property) {
+function plotData(fullData, property) {
   var margin = {top: 20, right: 20, bottom: 50, left: 40},
   width = 960 - margin.left - margin.right,
   height = 300 - margin.top - margin.bottom;
+
+  var data = pickData(fullData, property);
+  var filteredData = filterData(fullData, property);
 
   var x = d3.time.scale()
   .range([0, width]);
@@ -95,14 +177,15 @@ function plotData(data, property) {
   .orient("left")
   .ticks(10);
 
-  var svg = d3.select("body").append("svg")
+  d3.select("svg").remove();
+  var svg = d3.select(".graph").append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   x.domain(d3.extent(data, (function(d) { return d.date; })));
-  y.domain(d3.extent(data, (function(d) { return d[property]; })));
+  y.domain(d3.extent(data, (function(d) { return d.value; })));
 
   // Date axis
   var dateAxis = d3.svg.axis()
@@ -128,22 +211,47 @@ function plotData(data, property) {
   // Draw line
   var line = d3.svg.line()
     .defined(function(d) {
-      console.log(d);
-      return d[property] != null; })
+      return d.value != null; })
     .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d[property]); });
+    .y(function(d) { return y(d.value); });
 
   svg.append("path")
     .datum(data)
     .attr("class", "line")
     .attr("d", line);
 
+  svg.append("path")
+    .datum(filteredData)
+    .attr("class", "redline")
+    .attr("d", line);
+
   // Draw dots
   svg.selectAll(".dot")
-    .data(data.filter(function(d) { return d[property]; }))
+    .data(data.filter(function(d) {
+      return d.value;
+    }))
     .enter().append("circle")
     .attr("class", "dot")
     .attr("cx", line.x())
     .attr("cy", line.y())
-    .attr("r", 3.5);
+    .attr("r", 3);
 }
+
+
+// DOM handling
+function populateDropdown(columnNames) {
+  var $columnsDropdown = $("#columns");
+
+  columnNames.forEach(function(columnName) {
+    $columnsDropdown.append(
+      $("<option></option>").
+      attr("value", columnName).
+      text(columnName)
+    );
+  })
+
+  $columnsDropdown.change(function(event) {
+    var selectedColumn = $(event.target).val();
+    plotData(allData, selectedColumn);
+  })
+};
